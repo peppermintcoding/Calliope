b729e3c4b99fd4e417015473d4f991f8d296d4cd
Tim Betz <tim.betz@hs-augsburg.de>
2024-01-06 10:16:19 +0100

    added reading in txt as chunks

--- data/tokenize.py
+++ data/tokenize.py
@@ -2,21 +2,32 @@ from transformers import GPT2TokenizerFast
 import numpy as np
 
 
-def tokenize_txt(text_file: str, seq_len: int = 500):
+def tokenize_txt(text_file: str, seq_len: int = 500, out_filename: str = "train.bin"):
     """Tokenize txt file with GPT2 Tokenizer
     
     text_file: filename of the text file
     seq_len: sequence length (txt will be split on " ")
     """
-    with open(text_file, "r") as f:
-        full_text = f.read()
-    split_text = full_text.split(" ")
-
-    sequences = []
-    for i in range(0, len(split_text) - seq_len, seq_len):
-        sequences.append(" ".join(split_text[i : i + seq_len]))
-
     tokenizer = GPT2TokenizerFast.from_pretrained("gpt2")
-    input_ids = tokenizer(sequences, truncation=True)["input_ids"]
-    input_ids = np.concatenate(input_ids).astype(np.uint16).ravel()
-    return input_ids
+    first_save = True
+    with open(text_file) as f:
+        while True:
+            data = f.read(10**6)
+            if not data:
+                break
+            split_text = data.split(" ")
+
+            sequences = []
+            for i in range(0, len(split_text) - seq_len, seq_len):
+                sequences.append(" ".join(split_text[i : i + seq_len]))
+
+            input_ids = tokenizer(sequences, truncation=True)["input_ids"]
+            input_ids = np.concatenate(input_ids).astype(np.uint16).ravel()
+
+            if first_save:
+                np.save(out_filename, input_ids)
+                first_save = False
+            else:
+                with open(out_filename, "ab") as out_file:
+                    np.save(out_file, input_ids)
+    return True
