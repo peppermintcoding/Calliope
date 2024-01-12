c3cd67b6074043cf82da9a005841a0465e5b9900
Tim.Betz <tim.betz@man-es.com>
2024-01-12 15:13:06 +0100

    changed tokenzer form transformers to tiktoken

--- data/tokenize.py
+++ data/tokenize.py
@@ -1,37 +1,5 @@
 import numpy as np
 import tiktoken
-from transformers import GPT2TokenizerFast
-
-
-def tokenize_txt(text_file: str, seq_len: int = 500, out_filename: str = "train.bin"):
-    """Tokenize txt file with GPT2 Tokenizer
-
-    text_file: filename of the text file
-    seq_len: sequence length (txt will be split on " ")
-    """
-    tokenizer = GPT2TokenizerFast.from_pretrained("gpt2")
-    first_save = True
-    with open(text_file) as f:
-        while True:
-            data = f.read(10**6)
-            if not data:
-                break
-            split_text = data.split(" ")
-
-            sequences = []
-            for i in range(0, len(split_text) - seq_len, seq_len):
-                sequences.append(" ".join(split_text[i : i + seq_len]))
-
-            input_ids = tokenizer(sequences, truncation=True)["input_ids"]
-            input_ids = np.concatenate(input_ids).astype(np.uint16).ravel()
-
-            if first_save:
-                np.save(out_filename, input_ids)
-                first_save = False
-            else:
-                with open(out_filename, "ab") as out_file:
-                    np.save(out_file, input_ids)
-    return True
 
 
 def tokenize_txt_tiktoken(text_file: str, out_filename: str = "train.bin"):
@@ -40,7 +8,7 @@ def tokenize_txt_tiktoken(text_file: str, out_filename: str = "train.bin"):
     first_save = True
     with open(text_file) as f:
         while True:
-            data = f.read(10**6)
+            data = f.read(10**9)
             if not data:
                 break
             input_ids = np.array(tokenizer.encode(data)).astype(np.uint16)
