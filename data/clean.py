f7edc1500ce663322823adf555b480d696975345
Tim Betz <tim.betz@man-es.com>
2024-01-18 21:45:42 +0100

    changed from binary re pattern

--- data/clean.py
+++ data/clean.py
@@ -6,38 +6,38 @@ def filter_data(text: str) -> str:
     """Filters out weird things from the data"""
     patterns = [
         # pattern for more than 5 _ in a row
-        re.compile(b"_{5,}"),
+        re.compile("_{5,}"),
         # pattern for '.       .       .       .       .'
-        re.compile(b"\.\s{1,}\."),
+        re.compile("\.\s{1,}\."),
         # pattern for +++++++++++++
-        re.compile(b"\+{5,}"),
+        re.compile("\+{5,}"),
         # pattern for "        ."
-        re.compile(b"\s{3,}\."),
+        re.compile("\s{3,}\."),
         # pattern for "t t t t t t t t t t t t t t"
-        re.compile(b"t\s{3,}"),
+        re.compile("t\s{3,}"),
         # pattern for digit and newline
-        re.compile(b"\d\n"),
+        re.compile("\d\n"),
         # pattern for digit with . and newline
-        re.compile(b"\d\.\n"),
-        re.compile(b"Chorus:\n"),
-        re.compile(b"\xe2\x80\xa2"),
-        re.compile(b"\xe2\x80\x94"),
+        re.compile("\d\.\n"),
+        re.compile("Chorus:\n"),
+        re.compile("\xe2\x80\xa2"),
+        re.compile("\xe2\x80\x94"),
         re.compile(
-            b"\*\xc2\xa0\xc2\xa0\xc2\xa0\xc2\xa0\xc2\xa0\xc2\xa0\xc2\xa0\xc2\xa0"
+            "\*\xc2\xa0\xc2\xa0\xc2\xa0\xc2\xa0\xc2\xa0\xc2\xa0\xc2\xa0\xc2\xa0"
         ),
         re.compile(
-            b"\*\xc2\xa0\xc2\xa0\xc2\xa0\xc2\xa0\xc2\xa0\xc2\xa0 \*\xc2\xa0\xc2\xa0\xc2\xa0\xc2\xa0\xc2\xa0\xc2\xa0 *"
+            "\*\xc2\xa0\xc2\xa0\xc2\xa0\xc2\xa0\xc2\xa0\xc2\xa0 \*\xc2\xa0\xc2\xa0\xc2\xa0\xc2\xa0\xc2\xa0\xc2\xa0 *"
         ),
-        re.compile(b"\*\xc2\xa0\xc2\xa0\xc2\xa0 \*\xc2\xa0\xc2\xa0\xc2\xa0 \*"),
-        re.compile(b"\xe2\x9d\x96"),
-        re.compile(b"Refrain:\n"),
-        re.compile(b"\xc3\x9ea"),
-        re.compile(b"\xe2\x9d\x8f"),
-        re.compile(b"\xe2\x98\xbd"),
+        re.compile("\*\xc2\xa0\xc2\xa0\xc2\xa0 \*\xc2\xa0\xc2\xa0\xc2\xa0 \*"),
+        re.compile("\xe2\x9d\x96"),
+        re.compile("Refrain:\n"),
+        re.compile("\xc3\x9ea"),
+        re.compile("\xe2\x9d\x8f"),
+        re.compile("\xe2\x98\xbd"),
     ]
     for pat in patterns:
-        text = pat.sub(b"", text)
+        text = pat.sub("", text)
 
-    text = re.compile(b"&amp;").sub(b"and", text)
+    text = re.compile("&amp;").sub("and", text)
 
     return text
